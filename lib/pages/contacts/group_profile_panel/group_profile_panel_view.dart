5aac729ec3ce1d128d3d9fa63c1c93873c946e58
yang <418405228@qq.com>
2026-01-01 14:41:31 +0800

    fix([Group Information] Avatar displayed as square instead of circular)
    Signed-off-by: yang <418405228@qq.com>

--- lib/pages/contacts/group_profile_panel/group_profile_panel_view.dart
+++ lib/pages/contacts/group_profile_panel/group_profile_panel_view.dart
@@ -101,6 +101,7 @@ class GroupProfilePanelPage extends StatelessWidget {
               url: logic.groupInfo.value.faceURL,
               text: logic.groupInfo.value.groupName,
               isGroup: true,
+              isCircle: true,
               borderRadius: BorderRadius.circular(12.r),
             ),
             16.horizontalSpace,
@@ -235,9 +236,11 @@ class GroupProfilePanelPage extends StatelessWidget {
                 final member = logic.members.elementAt(index);
                 if (index == 5 && logic.members.length > 6) {
                   return Container(
+                    width: 44.w,
+                    height: 44.h,
                     decoration: BoxDecoration(
+                      shape: BoxShape.circle,
                       color: const Color(0xFFF3F4F6),
-                      borderRadius: BorderRadius.circular(10.r),
                     ),
                     child: Icon(
                       Icons.more_horiz_rounded,
@@ -249,6 +252,7 @@ class GroupProfilePanelPage extends StatelessWidget {
                 return AvatarView(
                   width: 44.w,
                   height: 44.h,
+                  isCircle: true,
                   text: member.nickname,
                   url: member.faceURL,
                   borderRadius: BorderRadius.circular(10.r),
